04a57bc99730d9a66accb53b105319995da51fb6
kalkikotha <kalki6309@gmail.com>
2021-04-23 12:20:40 +0530

    git init

---
+++ src/components/dashboard.js
@@ -0,0 +1,118 @@
+import React, { useState, useEffect } from 'react'
+import { useHistory, useParams } from 'react-router';
+import axios from 'axios';
+
+export default function Home() {
+
+    const history = useHistory();
+    const params = useParams()
+
+    let [fullUrl, setfullUrl] = useState("")
+    let [shortURL, setshortURL] = useState("")
+
+    let [data, setdata] = useState([]);
+
+    // useEffect(() => {
+    async function fetchData() {
+        let response = await axios.get(`http://localhost:5000/urls/${params.id}`, {
+            headers: {
+                Accept: 'application/json',
+                Authorization: window.localStorage.getItem('app_token')
+            }
+        })
+        return setdata(response.data.links);
+    }
+    // }, [params.id])
+
+    const handleSubmit = async (e) => {
+        e.preventDefault()
+        try {
+            let passdata = { fullUrl: fullUrl }
+            let res = await axios.post(`http://localhost:5000/urls/${params.id}`, passdata, {
+                headers: {
+                    Accept: 'application/json',
+                    Authorization: window.localStorage.getItem('app_token')
+                }
+            })
+            setdata([])
+            setshortURL(res.data.shortURL)
+            setfullUrl("");
+        }
+        catch (error) {
+            console.log(error)
+        }
+    }
+
+    const handleFetch = () => {
+        fetchData()
+    }
+
+    const handlelogout = () => {
+        window.localStorage.removeItem("app_token");
+        history.push('/login');
+    }
+
+    return (
+        <div className="container">
+            <h1 style={{
+                color: "white",
+                backgroundColor: "red",
+                padding: '20px'
+            }}>URL-Shrinker
+                <button style={{ float: 'right' }} onClick={handlelogout}>logout</button>
+                <button style={{ float: 'right', marginRight: '10px' }} onClick={handleFetch}>Fetch Data</button>
+            </h1>
+
+            <form onSubmit={handleSubmit} style={{ paddingTop: '20px' }}>
+                <label htmlFor="fullUrl" style={{ color: 'blue', fontWeight: 'bold' }}>Enter Url below</label>
+                <input placeholder="URL" type="url" name="fullUrl" value={fullUrl} onChange={(e) => setfullUrl(e.target.value)} />
+                <button type="submit">Shrink</button>
+            </form>
+
+            <h2 href="">{shortURL}</h2>
+
+
+            <table style={{
+                marginTop: '10px',
+                marginRight: 'auto',
+                marginLeft: 'auto',
+                border: "1px solid black",
+            }}>
+                <thead>
+                    <tr style={{
+                        border: "1px solid black"
+                    }}>
+                        <th style={{
+                            border: "1px solid black"
+                        }}>URL</th>
+                        <th style={{
+                            border: "1px solid black"
+                        }}>Shortened URL</th>
+                    </tr>
+                </thead>
+                {
+                    data.length !== 0 && data.map((data, index) => (
+                        <tbody>
+
+                            <tr style={{
+                                border: "1px solid black"
+                            }}>
+                                <td style={{
+                                    border: "1px solid black"
+                                }}>{data.longUrl} </td>
+                                <td style={{
+                                    border: "1px solid black"
+                                }}><a href="data.shortURL">{data.shortURL}</a></td>
+                            </tr>
+
+
+                        </tbody>
+                    ))
+                }
+            </table>
+
+
+
+        </div >
+    )
+}
---
+++ src/components/login.js
@@ -0,0 +1,117 @@
+import React, { useState } from 'react'
+import validator from 'validator'
+import axios from 'axios';
+import { useHistory } from 'react-router';
+import FontAwesome from 'react-fontawesome'
+
+export default function Login() {
+    const history = useHistory();
+    let [classname, setclassName] = useState("")
+    let [Name, setName] = useState("")
+    let [email, setemail] = useState("")
+    let [password, setpassword] = useState("")
+    let [passwordError, setpasswordError] = useState("")
+    let [emailerror, setemailerror] = useState("")
+    let [loginError, setLoginError] = useState("")
+    let [userlist, setuserlist] = useState([])
+
+    return (<>
+        <h2>Weekly Coding Challenge #1: Sign in/up Form</h2>
+        <div className={`container ${classname}`} id="container">
+            <div className="form-container sign-up-container">
+                <form action="#" onSubmit={async (e) => {
+                    e.preventDefault();
+
+                    if (validator.isEmail(email)) {
+                        setemailerror("Valid email :)")
+                    } else {
+                        return setemailerror("Enter valid email!")
+                    }
+
+                    if (validator.isStrongPassword(password)) {
+                        setpasswordError("Valid password")
+                    } else {
+                        return setpasswordError("Invalid password")
+                    }
+                    // }
+                    let data = {
+                        email: email, password: password
+                    }
+                    axios.post("http://localhost:5000/register", data).then((response) => {
+                        console.log(response);
+                    }).catch((err) => {
+                        console.log(err)
+                    });
+                }}>
+                    <h1>Create Account</h1>
+                    <div className="social-container">
+                        <a href="#" className="social"><i className="fa-facebook-f"></i></a>
+                        <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                        <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                    </div>
+                    <span>or use your email for registration</span>
+                    <input type="text" placeholder="Name" value={Name} required onChange={(e) => setName(e.target.value)} />
+                    <input type="email" placeholder="email" value={email} required onChange={(e) => setemail(e.target.value)} />
+                    <span>{emailerror}</span>
+                    <input type="password" placeholder="password" value={password} required onChange={(e) => setpassword(e.target.value)} />
+                    <span>{passwordError}</span>
+                    <button type="submit">Sign Up</button>
+                </form>
+            </div>
+            <div className="form-container sign-in-container">
+                <form action="#" onSubmit={async (e) => {
+                    e.preventDefault();
+                    if (validator.isEmail(email)) {
+                        setemailerror("Valid email :)")
+                    } else {
+                        return setemailerror("Enter valid email!")
+                    }
+                    let data = { email: email, password: password };
+                    axios.post("http://localhost:5000/login", data)
+                        .then((response) => {
+                            console.log(response)
+                            setuserlist(response.data)
+                            window.localStorage.setItem("app_token", response.data.token)
+                            if (response.data.message === "Allow") {
+                                history.push(`/dashboard/${response.data.id}`);
+                            }
+                            else {
+                                setLoginError("Incorrect Emailid or Password")
+                                history.push("/login");
+                            }
+                        }).catch((err) => console.log(err))
+                }}>
+
+                    <h1>Sign in</h1>
+                    <div className="social-container">
+                        <a href="#" className="social"><i className="fab fa-facebook-f"></i></a>
+                        <a href="#" className="social"><i className="fab fa-google-plus-g"></i></a>
+                        <a href="#" className="social"><i className="fab fa-linkedin-in"></i></a>
+                    </div>
+                    <span>or use your account</span>
+                    <input type="email" placeholder="email" value={email} required onChange={(e) => setemail(e.target.value)} />
+                    <span>{emailerror}</span>
+                    <input type="password" placeholder="password" value={password} required onChange={(e) => setpassword(e.target.value)} />
+                    <span>{loginError}</span>
+                    <a href="#">Forgot your password?</a>
+                    <button type="submit">Sign In</button>
+                </form>
+            </div>
+            <div className="overlay-container">
+                <div className="overlay">
+                    <div className="overlay-panel overlay-left">
+                        <h1>Welcome Back!</h1>
+                        <p>To keep connected with us please login with your personal info</p>
+                        <button className="ghost" id="signIn" onClick={() => { setclassName("") }}>Sign In</button>
+                    </div>
+                    <div className="overlay-panel overlay-right">
+                        <h1>Hello, Friend!</h1>
+                        <p>Enter your personal details and start journey with us</p>
+                        <button className="ghost" id="signUp" onClick={() => { setclassName("right-panel-active") }}>Sign Up</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </>
+    )
+}
